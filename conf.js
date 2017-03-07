91b608b1a5a55ebcc52f95ee057eee8006039ac7
Paulo Black <paulo.black@homes.com>
2017-03-07 16:00:46 -0500

    init

---
+++ conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: [
+        'test/**/*_spec.js'
+    ]
+  //   capabilities: {
+  //   'browserName': 'chrome'
+  // },
+};
---
+++ test/homepage/homepage_spec.js
@@ -0,0 +1,30 @@
+describe('HDC Home Page', function() {
+
+    beforeEach(function() {
+        browser.get('http://www.homes.com');
+    });
+
+    describe('Navbar', function() {
+        it('should have logo', function() {
+
+            //expect exists/visible
+            //expect text is correct
+            //if link expect href is correct
+            //if interactive/dynamic expect proper behavior
+            //if input expect proper behavior
+            //if children, recursively apply to children
+
+        });
+
+        it('should have menu', function() {
+
+        });
+
+        it('should have "Your Account"', function() {
+
+        });
+
+
+    })
+
+});
